e50dfe3f66c9c3b4076de725f1136ba8d764ef6a
saurabhkumardhiman <dsaurabh.kumar@gmail.com>
2024-11-29 13:03:40 +0530

    Metadata & Placeholder

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,6 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
+import { createOptimizedPicture, fetchPlaceholders } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -15,4 +15,15 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+  
+  // Placeholder
+  const placeholders = await fetchPlaceholders("");
+  const defaultPlaceholder = "";
+  const { viewTrips } = placeholders;
+  const viewTrip = document.querySelector(".section.cards-container .default-content-wrapper  p a");
+    if (viewTrip) {
+      viewTrip.textContent = viewTrips || defaultPlaceholder;
+  }
+
+
 }
